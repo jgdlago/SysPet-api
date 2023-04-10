cf7c40b8a6b2e9aa3cd72f0689ea01c934512cee
João Dal Lago <joaogabrieldallago@hotmail.com>
2023-04-10 10:41:41 -0300

    init

---
+++ petshopSystem/src/test/java/com/petshopSystem/petshopSystem/PetshopSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshopSystem.petshopSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
