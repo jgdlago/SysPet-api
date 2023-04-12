590df58f195372db98d6eb4324bac9caa8d60d25
João Dal Lago <joaogabrieldallago@hotmail.com>
2023-04-12 09:56:30 -0300

    Add controllers package

---
+++ petshopSystem/src/main/java/com/petshopSystem/controllers/PetshopController.java
@@ -0,0 +1,10 @@
+package com.petshopSystem.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/petshop")
+public class PetshopController {
+
+}
--- petshopSystem/src/main/java/com/petshopSystem/repositories/PetShopRepository.java
+++ petshopSystem/src/main/java/com/petshopSystem/repositories/PetShopRepository.java
@@ -1,9 +1,14 @@
 package com.petshopSystem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.petshopSystem.entities.Petshop;
 
+@Repository
 public interface PetShopRepository extends JpaRepository<Petshop, Long>{
-
+	
+	List<Petshop> findByNameContainingIgnoreCase(String name);
 }
