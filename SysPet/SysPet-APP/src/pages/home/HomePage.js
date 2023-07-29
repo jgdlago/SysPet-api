545dc08b39778f20a6c3343a5c6d0178182c5734
João Dal Lago <joaogabrieldallago@hotmail.com>
2023-07-29 06:03:33 -0300

    Added a welcome screen to be used as system default

--- SysPet/SysPet-APP/src/pages/home/HomePage.js
+++ SysPet/SysPet-APP/src/pages/home/HomePage.js
@@ -1,13 +1,18 @@
+import React, { useState } from 'react';
 import SidebarMenu from '../../components/menu/SidebarMenu';
 import WelcomeScreen from '../../components/welcomeScreen/WelcomeScreen';
-import '../../styles.css'
-import './homePage.css'
+import '../../styles.css';
+import './homePage.css';
 
 function HomePage() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
   return (
     <div className="homePage-container">
-      <SidebarMenu />
-      <WelcomeScreen />
+      <SidebarMenu menuVisible={menuVisible} />
+      <div className={`content-container ${menuVisible ? "menu-open" : ""}`}>
+        <WelcomeScreen />
+      </div>
     </div>
   );
 }
